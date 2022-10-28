db04fbfd90de21c5f0559db09821fddbcc39849e
Walker <walker@praiseweb.com.br>
2022-10-28 16:25:49 -0300

    build: fix nix check

--- flake.nix
+++ flake.nix
@@ -8,45 +8,51 @@
     };
 
   outputs = { utils, ... }@inputs:
-    utils.apply-systems { inherit inputs; } ({ pkgs, system, ... }:
-      let
-        ps-tools = inputs.ps-tools.legacyPackages.${system};
-        purs-nix = inputs.purs-nix { inherit system; };
-        ps =
-          purs-nix.purs
-            {
-              dependencies =
-                with purs-nix.ps-pkgs;
-                [
-                  effect
-                  prelude
-                  node-process
-                ];
-
-              dir = ./.;
-            };
-      in
-      rec {
-        apps.default = {
-          type = "app";
-          program = "${packages.default}/bin/purs-eval";
-        };
-
-        packages = with ps.modules.Main; {
-          default = app { name = "purs-eval"; };
-          bundle = bundle { };
-          output = output { };
-        };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            (ps.command { })
-            ps-tools.for-0_15.purescript-language-server
-            purs-nix.esbuild
-            purs-nix.purescript
-          ];
-        };
+    utils.apply-systems
+      {
+        inherit inputs;
+        # Limited by ps-tools
+        systems = [ "x86_64-linux" "x86_64-darwin" ];
       }
-    );
+      ({ pkgs, system, ... }:
+        let
+          ps-tools = inputs.ps-tools.legacyPackages.${system};
+          purs-nix = inputs.purs-nix { inherit system; };
+          ps =
+            purs-nix.purs
+              {
+                dependencies =
+                  with purs-nix.ps-pkgs;
+                  [
+                    effect
+                    prelude
+                    node-process
+                  ];
+
+                dir = ./.;
+              };
+        in
+        rec {
+          apps.default = {
+            type = "app";
+            program = "${packages.default}/bin/purs-eval";
+          };
+
+          packages = with ps.modules.Main; {
+            default = app { name = "purs-eval"; };
+            bundle = bundle { };
+            output = output { };
+          };
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+              (ps.command { })
+              ps-tools.for-0_15.purescript-language-server
+              purs-nix.esbuild
+              purs-nix.purescript
+            ];
+          };
+        }
+      );
 }
