b84d26775a47058099d69be2d02dad423663b5a0
Walker <walker@praiseweb.com.br>
2022-10-28 21:13:54 -0300

    feat: add compile

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,23 @@
 module Main where
 
-import Prelude (Unit, void, ($))
+import Prelude (Unit, ($), (<<<), void, pure, bind)
 
+import Control.Monad.Error.Class (liftEither)
+import Data.Bifunctor (lmap)
+import Data.Maybe (Maybe(Just))
 import Effect (Effect)
+import Effect.Aff (Aff, error)
 import Node.Process (stdin, stdout)
 import Node.Stream (pipe)
+import Affjax.ResponseFormat (string)
+import Affjax.Node (post, printError)
+import Affjax.RequestBody (RequestBody (String))
+
+compile :: String -> Aff String
+compile s = do
+  mRes <- post string "http://localhost:3000/compiler" $ Just $ String s
+  res <- liftEither $ (error <<< printError) `lmap` mRes 
+  pure res.body
 
 main :: Effect Unit
 main = void $ stdin `pipe` stdout
