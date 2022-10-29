4fc3826b7b85ca18eaac4dac62a7232043d64ce2
Walker <walker@praiseweb.com.br>
2022-10-29 13:26:19 -0300

    test: add compile

--- test/Main.purs
+++ test/Main.purs
@@ -1,20 +1,36 @@
 module Test.Main where
 
-import Prelude (Unit, ($), unit, pure, bind, const)
+import Prelude (Unit, ($), (==), mempty, unit, pure, bind, const)
+import Control.Bind ((>=>))
+import Control.Apply ((*>))
+import Control.Monad.Error.Class (class MonadThrow, throwError, liftMaybe)
 import Effect
 import Effect.Aff (Aff, bracket)
 import Effect.Class (liftEffect)
+import Effect.Exception (Error, error)
+import HTTPure.Request (Request)
 import HTTPure.Response (ok)
 import HTTPure.Server (serve)
 import HTTPure.Body (class Body)
+import HTTPure.Path (read)
+import HTTPure.Lookup ((!!))
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
 import Test.Unit.Assert (equal)
 import Main (compile)
 
+validateReq :: forall m. MonadThrow Error m => Request -> m Unit
+validateReq req = do
+   let invalidPath = error "invalid path"
+       missingPath = error "missing path"
+   p <- liftMaybe missingPath $ read req !! 0
+   case p == "/compile" of
+        true -> pure unit
+        false -> throwError invalidPath 
+
 mockSrv :: forall a. Body a => a -> Aff (Effect Unit)
 mockSrv res = do
-  close <- liftEffect $ serve 3000 (const $ ok res) $ pure unit
+  close <- liftEffect $ serve 3000 (\req -> validateReq req *> ok res) $ pure unit
   pure $ close $ pure unit
 
 setupSrv :: forall a b. Body a => a -> Aff b -> Aff b
