709298547ac19be29cb73ebefb29c8b36235c72f
Walker <walker@praiseweb.com.br>
2022-11-01 21:16:20 -0300

    feat: add try pparsing

--- src/Main.purs
+++ src/Main.purs
@@ -6,13 +6,18 @@ import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Node.Process (stdin, stdout)
 import Node.Stream.Aff (readSome, write, end, toStringUTF8, fromStringUTF8)
-import Compiler (runCompiler)
+import Compiler (successFromJson, runCompiler)
 
 main :: Effect Unit
 main = launchAff_ do
   Tuple input _ <- readSome stdin
   code <- toStringUTF8 input
-  code' <- runCompiler { protocol: "https", hostname: "compile.purescript.org", port: 443 } code
+  let settings = { protocol: "https"
+                 , hostname: "compile.purescript.org"
+                 , port: 443
+                 , parser: successFromJson
+                 }
+  code' <- runCompiler settings code
   output <- fromStringUTF8 code'
   write stdout output
   end stdout
